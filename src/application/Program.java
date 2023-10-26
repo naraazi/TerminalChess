3d44d75ae6a185a33e02d6234f80fa2fbfa0d8dc
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2023-10-26 16:27:32 -0300

    Classe 'Position' adicionada

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position p = new Position(1, 2);
+
+        System.out.println(p.toString());
+    }
+}
