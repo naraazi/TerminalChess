f5bf742581b0726345900f13593ebfa29db0250b
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2023-10-27 14:51:04 -0300

    Inicio da implementação de 'Board' e 'Piece'

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,9 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
     public static void main(String[] args) {
-        Position p = new Position(1, 2);
-
-        System.out.println(p.toString());
+        Board board = new Board(8, 8);
     }
 }
