5fbb278b39c8601d2cdb1c59323ac718ef320d6a
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2023-11-02 13:27:12 -0300

    Xequemate implementado

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,7 +16,7 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -44,5 +44,7 @@ public class Program {
             }
         }
 
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
