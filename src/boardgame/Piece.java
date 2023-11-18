beba9c5345c2eb5ebef532674a35204232279fbc
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2023-11-18 10:30:31 -0300

    Refatoração 6: Refinando classe 'Piece'

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,7 +2,7 @@ package boardgame;
 
 public abstract class Piece {
     protected Position position;
-    private Board board;
+    private final Board board;
 
     public Piece(Board board) {
         this.board = board;
@@ -22,9 +22,9 @@ public abstract class Piece {
     public boolean isThereAnyPossibleMove() {
         boolean[][] mat = possibleMoves();
 
-        for (int i = 0; i < mat.length; i++) {
+        for (boolean[] booleans : mat) {
             for (int j = 0; j < mat.length; j++) {
-                if (mat[i][j]) {
+                if (booleans[j]) {
                     return true;
                 }
             }
