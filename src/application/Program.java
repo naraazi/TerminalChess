d5caf697278082bc4638dc2cb49fc8939fdb9b8d
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2023-11-18 10:20:47 -0300

    Refatoração 3: Refinando classe 'Program'

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,6 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
